5dd28659b8be6b5f0352e7eafe769f9795f46011
ArpanGyawali <arpan.gyawali7@gmail.com>
2021-04-27 21:46:45 +0545

    added no items

--- src/Components/CardList.js
+++ src/Components/CardList.js
@@ -5,19 +5,27 @@ const CardList = ( {movies} ) => {
 	// if (true) {					for error throw..only for App production
 	// 	throw new Error('Error');
 	// }
-	const cardArray = movies.map((movie, i) => {
-		return <Card key={i} 
-					 image={movies[i].image} 
-					 name={movies[i].name} 
-					 year={movies[i].year} 
-					 rating={movies[i].rating} 
-					 search={movies[i].link} />
-	})
-	return (
-		<div>
-	    	{cardArray}
-	    </div>
-	);
+	if (movies.length === 0) {
+			return <h1 className='gray tc'> No result found </h1>
+		}
+	else {
+		const cardArray = movies.map((movie, i) => {
+			return <Card key={i} 
+						 image={movies[i].image} 
+						 name={movies[i].name} 
+						 year={movies[i].year} 
+						 rating={movies[i].rating} 
+						 search={movies[i].link} 
+					/>
+
+		})
+
+		return (
+			<div>
+		    	{cardArray}
+		    </div>
+		);
+	}
 }
 
 export default CardList;
